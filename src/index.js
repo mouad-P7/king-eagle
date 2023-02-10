39cd1f5b44979a78f16f64e41a2198bbf1a9ffde
mouad-p7 <mouadananouch7@gmail.com>
2023-02-10 21:06:49 +0100

    Add welcome, swipe, intro and services section.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import './scss/style.scss';
 
 // import images
-import "./images/logo.png";
+import './images/logo.png';
+import './images/welcome.jpg';
+import './images/service1.png';
+import './images/service2.png';
+import './images/service3.png';
 
 
