6684b2a4df3fd9d28854c641c060d841dc1d635c
mouad-p7 <mouadananouch7@gmail.com>
2023-02-10 22:58:55 +0100

    Add swipe gif and examples section.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './scss/style.scss';
 // import images
 import './images/logo.png';
 import './images/welcome.jpg';
+import './images/swipe.gif';
 import './images/service1.png';
 import './images/service2.png';
 import './images/service3.png';
-
+import './images/vfx.jpg';
+import './images/cover.jpg';
+import './images/affiche.jpg';
+import './images/charte.jpg';
+import './images/brochure.jpg';
+import './images/panneau.jpg';
 
