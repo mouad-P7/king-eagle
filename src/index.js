421181c50f284beeb39db18cb965cbfd6489b3f2
mouad-p7 <mouadananouch7@gmail.com>
2023-03-27 09:44:33 +0000

    Adding the final touche.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ import './scss/style.scss';
 import './images/logo.png';
 import './images/bg-portrait.jpg';
 import './images/bg-landscape.jpg';
-import './images/side-img-1.jpeg';
-import './images/side-img-2.jpeg';
-import './images/side-img-3.jpeg';
-import './images/side-img-4.jpeg';
+import './images/gallery-img-1.jpeg';
+import './images/gallery-img-2.jpeg';
+import './images/gallery-img-3.jpeg';
+import './images/gallery-img-4.jpeg';
+import './images/gallery-img-5.jpeg';
+import './images/gallery-img-6.jpeg';
+import './images/gallery-img-7.jpeg';
+import './images/gallery-img-8.jpeg';
 import './images/graphic-design.jpeg';
 import './images/video-creator.jpeg';
 import './images/photographer.jpeg';
@@ -21,5 +25,6 @@ import './images/carrefour.png';
 import './images/personal-img.jpg';
 
 // import videos
+import './videos/side-video-4.mp4';
 import './videos/pitch-video.mp4';
 
